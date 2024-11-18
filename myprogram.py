93aed6e0306805c206eb782b50967474d6f67c53
Anna <annadonizak@wp.pl>
2024-11-18 11:24:49 +0100

    Add files via upload

---
+++ myprogram.py
@@ -0,0 +1,11 @@
+r = 4
+area = 3.14159 * r * r
+print("Circle area:", round(area, 2))
+
+a = 5
+h = 4
+triangle_area = 0.5 * a * h
+print("Triangle area:", triangle_area)
+
+mean = (7 + 12 + 31) / 3
+print("Mean:", round(mean, 2))

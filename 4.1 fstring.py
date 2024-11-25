1e45fe5ecf29d53e9b1e7ef77bcb4f301888052d
annadonizak <annadonizak@wp.pl>
2024-11-25 12:29:52 +0100

    personal data

---
+++ 4.1 fstring.py
@@ -0,0 +1,14 @@
+###
+# Printing student's personal data
+#
+name = "Adam"
+age = 50
+h = 180
+future_age = 50 + 6
+
+print(f"""
+My name is {name}.
+I am {age} years old, and my height is {h} cm.
+In 6 years I will be {future_age} years old.  
+""")
+

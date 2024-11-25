fe5538ed1512767386a9c12d96baef880a49338a
annadonizak <annadonizak@wp.pl>
2024-11-25 12:04:34 +0100

    Prints the speeed in m/s

--- 3.4 speed.py
+++ 3.4 speed.py
@@ -1,5 +1,11 @@
 ###
+# ###
 # A program that, for a given speed in km/h,
+# prints the speed in m/s
+#
+speed_kmh = 70
+speed_ms = 70/3.6
+print(speed_kmh, "km/h = ", speed_ms, "m/s")
 # prints the speed in m/s
 #
 speed_kmh = 70
